ec4178e47aacd926299c6b328f72e3e433d16196
BigAaron1243 <bigaaron1243@gmail.com>
2023-03-26 17:26:05 +1100

    tutorial 1 complete

--- main.cpp
+++ main.cpp
@@ -1,16 +1,110 @@
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
-
+#include "linmath.h"
 #include <iostream>
-#include "loader.h"
+
+
+struct vattr
+{
+    float x, y;
+    float r, g, b;
+}; 
+
+struct vattr vertices[9] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f },
+    { 1.f, 1.f, 0.f, 1.f, 1.f},
+    { 1.f, 0.f, 1.f, 0.f, 1.f},
+    { 0.f, 1.f, 1.f, 1.f, 0.f},
+    { 0.f, 0.2f, 1.f, 1.f, 0.f},
+    { 0.2f, 0.f, 1.f, 0.f, 1.f},
+    { -1.f, 1.f, 0.f, 1.f, 1.f}
+};
+
+
+struct vattr vertices2[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f },
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+
+
+void error_callback(int error, const char* description) {
+    std::cerr << "Error: " << description;
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+
 
 int main(int argc, char *argv[]) {
-    GLFWwindow* window = lInitGLFW();   
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
-    while (!glfwWindowShouldClose(window)) {
-        
+
+
+    // Init glfw
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to init glfw\n";
+        return -1;
     }
-    lTerminateGLFW(window);
+    glfwSetErrorCallback(error_callback);
+
+    // Set window hints
+    glfwWindowHint(GLFW_SAMPLES, 4); //4x antialiasing
+    
+    // Create window and make it current
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Collisions", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to open glfw window";
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Load openGL
+    gladLoadGL();
+
+    // Set key callback
+    glfwSetKeyCallback(window, key_callback);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        // Clear Screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
